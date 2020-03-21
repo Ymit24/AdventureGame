ec8aa28bde4065bcca103487b3d6dff6e5c9fb62
Christian Smith <epiceditiongames@gmail.com>
2020-03-21 10:44:46 -0500

    Added server project :: rest of files

--- AdventureGameServer/AdventureGameServer/Program.cs
+++ AdventureGameServer/AdventureGameServer/Program.cs
@@ -1,9 +1,9 @@
 ﻿namespace AdventureGameServer
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+        }
     }
-  }
 }
